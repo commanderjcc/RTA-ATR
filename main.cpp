7740a1242b8c4ca046293bd8855a8675e16811ad
commanderjcc <jj1355810@gmail.com>
2020-02-10 19:36:52 -0500

    Create functions and do-while loop

--- main.cpp
+++ main.cpp
@@ -5,8 +5,33 @@
 // Date: 2/10/20
 
 #include <iostream>
+using namespace std;
+
+void RTA() {
+    char input[16];
+    int count = 0;
+
+}
+
+void ATR() {
+    char
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int choice = 0;
+    do {
+        cout << "What would you like to do?\n"
+             << "1: Roman to Arabic\n"
+             << "2: Arabic to Roman\n"
+             << "[Enter a number]: ";
+        cin >> choice;
+
+        if (choice == 1) {
+            RTA();
+        } else {
+            ATR();
+        }
+
+    } while (choice != 0);
 }
